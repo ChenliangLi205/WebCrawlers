2cd87ba0f45a1535c6739dc34564208cf730bc70
ChenliangLi205 <chenliangli@nuaa.edu.cn>
2018-07-17 12:45:40 +0800

    Adding a basic framework for web crawling Add Douban crawler based on the basic model

---
+++ Basic.py
@@ -0,0 +1,101 @@
+#-*- coding:utf-8 -*-
+import urllib.parse
+import urllib.request
+import urllib.error
+import time
+from collections import deque
+import ssl
+
+ssl._create_default_https_context = ssl._create_unverified_context
+
+
+class BasicCrawler(object):
+    """
+    基本的爬虫小框架， 用的话只要把process_page_data这个函数重写了就行
+    """
+    def __init__(self):
+        self.headers = {
+            'Connection': 'keep-alive',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+            'Accept-Encoding': 'sdch, br',
+            'Accept-Language': 'zh-CN,zh;q=0.8',
+            'User-Agent':
+                'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'
+        }
+        self.url_queue = deque()
+        self.crawl_count = 0
+        self.visited = set()
+        self.current_url = None
+        self.time_sleep = 3600
+
+    def start_with(self, url):
+        """
+        :param url: str or list, the url(s) to start with
+        :return:
+        """
+        if isinstance(url, str):
+            self.url_queue.append(url)
+            return
+        if isinstance(url, list):
+            for u in url:
+                if isinstance(u, str):
+                    self.url_queue.append(u)
+                else:
+                    raise TypeError("Invalid Url Type")
+            return
+        raise TypeError("Invalid Url Type")
+
+    def request(self):
+        """
+        :return: the request
+        """
+        request = urllib.request.Request(
+            url=self.current_url,
+            data=None,
+            headers=self.headers
+        )
+        return request
+
+    def retrieve(self):
+        """
+        :return: the page data
+        """
+        try:
+            data = urllib.request.urlopen(self.request()).read().decode('utf-8')
+        except:
+            print('failed to open url ' + self.current_url)
+            print('probably ip blocked, trying again in one hour...')
+            time.sleep(self.time_sleep)
+            self.url_queue.appendleft(self.current_url)
+            return
+        return data
+
+    def get_page_data(self):
+        """
+        get page data
+        """
+        if len(self.url_queue):
+            self.current_url = self.url_queue.popleft()
+
+            if self.current_url not in self.visited:
+                print('crawling url ' + self.current_url + ' ' + str(len(self.visited)) + ' urls crawled')
+                self.visited.add(self.current_url)
+                return self.retrieve()
+            else:
+                return
+        else:
+            return
+
+    def run(self):
+        """
+        Bu Zhi Dao Gai Gan Shen Me
+        """
+        page_data = self.get_page_data()
+        while page_data:
+            self.process_page_data(page_data)
+            page_data = self.get_page_data()
+        return
+
+    def process_page_data(self, page_data):
+        """sha ye bu gan"""
+        pass
---
+++ douban_crawler.py
@@ -0,0 +1,198 @@
+#-*- coding:utf-8 -*-
+from pyquery import PyQuery as pq
+import networkx as nx
+import argparse
+import re
+from Basic import BasicCrawler
+
+
+def parse_args():
+    """
+    parse the arguments
+    """
+    parser = argparse.ArgumentParser(description="Run Douban Movie Crawler.")
+
+    parser.add_argument(
+        '--starting-url', nargs='+', help='Input the url(s) to start with.',
+        default='https://movie.douban.com/subject/1292052/?from=subject-page'
+    )
+    parser.add_argument(
+        '--path', nargs='?', help='Input the path to save graph file and label file.',
+        default='Data/Douban/'
+    )
+    parser.add_argument(
+        '--max-size', type=int, default=200, help='The approximate number of nodes in the network'
+    )
+    return parser.parse_args()
+
+
+class DoubanCrawler(BasicCrawler):
+    def __init__(self, max_size):
+        """
+        :param max_size: maximum amount of nodes in the graph
+        """
+        super(DoubanCrawler, self).__init__()
+        self.max_size = max_size
+        self.count = 0
+        self.styles = ["爱情片", "动作片", "战争片", "犯罪片",
+                     "动画片", "悬疑片", "喜剧片", "科幻片"]
+        self.count_tags = {}
+        self.node_tags = {}
+        for s in self.styles:
+            self.count_tags[s] = 0
+        self.graph = nx.Graph()
+        self.attr_graph = nx.Graph()
+
+    def start_with(self, url):
+        """
+        :param url: str or list, the url(s) to start with
+        """
+        if isinstance(url, str):
+            self.url_queue.append(url)
+            return
+        if isinstance(url, list):
+            for u in url:
+                if isinstance(u, str):
+                    self.url_queue.append(u)
+                else:
+                    raise TypeError("Invalid Url Type")
+            return
+        raise TypeError("Invalid Url Type")
+
+    def process_page_data(self, page_data):
+        """
+        fetch style for the current movie
+        get the movies that are linked to the current one
+        """
+        styled = self.styling(page_data)
+        if styled:
+            self.linking(page_data)
+            # self.tagging(page_data)
+
+    @staticmethod
+    def get_linked_url(page_data):
+        """ get the linked urls of the current one"""
+        url_dd = pq(page_data)('body').find('div').filter('.recommendations-bd')('dd')
+        urls = []
+        i = 0
+        url_a = url_dd.eq(i)
+        while url_a:
+            url = url_a('a').attr('href')
+            urls.append(url)
+            i += 1
+            url_a = url_dd.eq(i)
+        return urls
+
+    @staticmethod
+    def get_tags(page_data):
+        """
+        fetch tag for the current movie
+        """
+        label_a = pq(page_data)('body').find('div').filter('.tags-body')('a')
+        labels = []
+        i = 0
+        label_i = label_a.eq(i)
+        while label_i:
+            tag = label_i.text()
+            labels.append(tag)
+        return labels
+
+    @staticmethod
+    def get_genres(page_data):
+        """
+        fetch genre of the movie
+        """
+        genres = []
+        doc = pq(page_data)
+        items = doc('.rating_betterthan')
+        a_s = items.find('a').text()
+        r = re.compile(r'% .+?片')
+        for ele in r.findall(a_s):
+            genre = ele.replace('% ', '')
+            genres.append(genre)
+        return genres
+
+    def linking(self, page_data):
+        """
+        get the movies that are linked to the current one
+        """
+        urls = self.get_linked_url(page_data)
+        max_size = self.max_size
+        graph = self.graph
+        for u in urls:
+            if graph.number_of_nodes() > max_size:
+                break
+            if u in self.visited:
+                continue
+            if u not in graph.nodes():
+                self.url_queue.append(u)
+            graph.add_edge(u, self.current_url)
+
+    def styling(self, page_data):
+        """
+        fetch genre for the current movie
+        """
+        styles = self.styles
+        count_tags = self.count_tags
+        current_url = self.current_url
+        genres = self.get_genres(page_data)
+        for g in genres:
+            if g in styles:
+                print(current_url + ' is ' +g)
+                self.node_tags[current_url] = g
+                count_tags[g] += 1
+                return True
+        print('Cannot get the type of this movie')
+        self.graph.remove_node(current_url)
+        return False
+
+    def tagging(self, page_data):
+        """
+        fetch tags of the current movie
+        and adding edges and nodes into the attributed graph
+        """
+        attr_graph = self.attr_graph
+        current_url = self.current_url
+        tags = self.get_tags(page_data)
+        for t in tags:
+            attr_graph.add_edge(current_url, t)
+
+    def show_tag_num(self):
+        """
+        Show the current amount of movies in each class
+        """
+        count_tags = self.count_tags
+        print('# of nodes', self.graph.number_of_nodes())
+        print('# of links', self.graph.number_of_edges())
+        for key, count in count_tags.items():
+            print(key, count)
+
+    def save(self, path):
+        """
+        Saving the movie graph and attributed graph and node labels into three files
+        """
+        graph_file = 'graph.edgelist'
+        attrgraph_file = 'attrgraph.edgelist'
+        nodelabel_file = 'labels.txt'
+        node_tags = self.node_tags
+        graph = self.graph
+        nx.write_edgelist(G=graph, path=path+graph_file, data=False)
+        nx.write_edgelist(G=graph, path=path+attrgraph_file, data=False)
+        with open(path+nodelabel_file, 'w') as f:
+            for node in graph.nodes():
+                f.writelines(str(node)+' '+str(node_tags[node])+'\n')
+
+
+if __name__ == "__main__":
+    """
+    To run douban crawler:
+
+    python douban_crawler.py --starting-url (The url of page to start with) --max-size 20 --path (The path to put files in)
+
+    """
+    args = parse_args()
+    crawler = DoubanCrawler(args.max_size)
+    crawler.start_with(args.starting_url)
+    crawler.run()
+    crawler.show_tag_num()
+    crawler.save(path=args.path)
